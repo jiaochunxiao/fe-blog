0335dd08352b115207e0b10c42efec522595dcc1
jiaochunxiao <jcx13627607951@gmail.com>
2020-09-21 14:13:41 +0800

    文档: shell 特殊变量

--- shell/demo/run.sh
+++ shell/demo/run.sh
@@ -1,47 +1,79 @@
-!/bin/bash
-echo "Hello World!"
-your_name="helloworld"
-unset your_name
-echo $your_name
-echo ${your_name}
-for skill in Ada Coffee Action Java; do
-  echo "I am goot at ${skill}Script"
+#!/bin/bash
+# echo "Hello World!"
+# your_name="helloworld"
+# unset your_name
+# echo $your_name
+# echo ${your_name}
+# for skill in Ada Coffee Action Java; do
+#   echo "I am goot at ${skill}Script"
+# done
+
+# your_name="shell"
+# # 使用双引号拼接
+# greeting="Hello, "$your_name" !"
+# greeting1="Hello, ${your_name} !"
+
+# echo $greeting $greeting1
+
+# # 使用单引号拼接
+# greeting2='Hello, '$your_name' !'
+# greeting3='Hello, ${your_name} !' # 不能输出想要的数据
+
+# echo $greeting2 $greeting3
+
+# echo ${#your_name}
+
+# string="Hello World"
+# echo ${string:1:4}
+# # 指定shell类型
+
+# echo -n "Enter your name:"                   # 参数-n的作用是不换行，echo默认换行
+# read  name                                   # 把键盘输入放入变量name
+# echo "hello $name,welcome to my program"     # 显示输入信息
+
+# currenDir=$PWD
+# echo $currenDir
+
+
+# echo -n "Enter your name:"                   # 参数-n的作用是不换行，echo默认换行
+# read  name                                   # 把键盘输入放入变量name
+# echo "hello $name,welcome to my program"     # 显示输入信息
+# echo -n "Enter your name:"
+# read  word
+# echo "hello $word,welcome to my program"
+
+echo "\$*"= $*
+echo "\"\$*\"=" "$*"
+
+echo "\$@"= $@
+echo "\"\$@\"=" "$@"
+
+echo "print each param from \$*"
+
+for var in $*
+do
+  echo "$var"
 done
 
-your_name="shell"
-# 使用双引号拼接
-greeting="Hello, "$your_name" !"
-greeting1="Hello, ${your_name} !"
+echo "print each param from \$@"
 
-echo $greeting $greeting1
+for var in $@
+do
+  echo "$var"
+done
 
-# 使用单引号拼接
-greeting2='Hello, '$your_name' !'
-greeting3='Hello, ${your_name} !' # 不能输出想要的数据
+echo "print each param from \"\$*\""
 
-echo $greeting2 $greeting3
+for var in "$*"
+do
+  echo "$var"
+done
 
-echo ${#your_name}
+echo "print each param from \"\$@\""
 
-string="Hello World"
-echo ${string:1:4}
-# 指定shell类型
-
-echo -n "Enter your name:"                   # 参数-n的作用是不换行，echo默认换行
-read  name                                   # 把键盘输入放入变量name
-echo "hello $name,welcome to my program"     # 显示输入信息
-exit 0
-
-currenDir=$PWD
-echo $currenDir
-
-
-echo -n "Enter your name:"                   # 参数-n的作用是不换行，echo默认换行
-read  name                                   # 把键盘输入放入变量name
-echo "hello $name,welcome to my program"     # 显示输入信息
-echo -n "Enter your name:"
-read  word
-echo "hello $word,welcome to my program"
-exit 0
+for var in "$@"
+do
+  echo "$var"
+done
 
 
